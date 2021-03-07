6675d9e40114fbe695762291c448bc34e2049e6d
Albert Lin <aklin@princeton.edu>
2021-03-06 20:34:57 -0500

    initial model designs

--- TRT-django/marketplace/models.py
+++ TRT-django/marketplace/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Item(models.Model):
+    AVAILABLE = 0
+    FROZEN = 1
+    COMPLETE = 2
+
+    seller = models.ForeignKey(UserProfile)
+    name = models.CharField(max_length=50)
+    posted_date = models.DateTimeField()
+    deadline = models.DateTimeField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quality = models.DecimalField(max_digits=1, decimal_places=0)
+    categories = models.ManyToManyField(Category)
+    description = models.TextField()
+    image = models.ImageField()
+    status = models.DecimalField(max_digits=1, decimal_places=0, 
+        choices = [
+            (AVAILABLE, 'available'),
+            (FROZEN, 'frozen'),
+            (COMPLETE, 'complete'),
+        ])
+
+class Transaction(models.Model):
+    INITIATED = 0
+    ACKNOWLEDGED = 1
+    S_PENDING = 2
+    B_PENDING = 3
+    COMPLETE = 4
+
+    item = models.ForeignKey(Item)
+    seller = models.ForeignKey(UserProfile)
+    buyer = models.ForeignKey(UserProfile)
+    status = models.DecimalField(max_digits=1, decimal_places=0,
+        choices = [
+            (INITIATED, 'initiated'),
+            (ACKNOWLEDGED, 'acknowledged'),
+            (S_PENDING, 's_pending'),
+            (B_PENDING, 'b_pending'),
+            (COMPLETE, 'complete'),
+        ]
+    )
