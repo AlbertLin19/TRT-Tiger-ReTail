58d4b3a52cd104159b9259ecc19ab69d6eea0c10
Phoebe Lin <PhoebeLin2001@gmail.com>
2021-03-21 22:43:42 -0400

    fix gallery

--- TRT-django/marketplace/static/marketplace/functions.js
+++ TRT-django/marketplace/static/marketplace/functions.js
@@ -1,6 +1,9 @@
 function search(){
    var ret_items = []
    query = document.getElementById("searchbar").value;
+   item=[{% for item in items%}
+      {{item.name},
+   {% end for}]
    if (query == null)
       return items;
    for (i = 0; i < items.length; i++) {
