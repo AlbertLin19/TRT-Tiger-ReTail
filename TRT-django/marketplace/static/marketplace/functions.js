015716c5e2924d7b4eef4e4e2bf699b6e5b16212
Phoebe Lin <PhoebeLin2001@gmail.com>
2021-03-25 02:32:50 -0400

    my items fix

--- TRT-django/marketplace/static/marketplace/functions.js
+++ TRT-django/marketplace/static/marketplace/functions.js
@@ -1,15 +1,15 @@
 function search(){
    var ret_items = []
    query = document.getElementById("searchbar").value;
-   item=[{% for item in items%}
-      {{item.name},
-   {% end for}]
+   /*
+   item=[{% for item in items %}
+            {{item.name}, }
+         {% endfor %}]
    if (query == null)
-      return items;
+      return items; */
    for (i = 0; i < items.length; i++) {
       if(query.equals(items[i])) {
          ret_items.push(items[i]);
       }
    }
-   return ret_items;
 }
