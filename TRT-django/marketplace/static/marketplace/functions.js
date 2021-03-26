1ebb63ab156f401d45b8f400c3ffbae396af4f3c
Phoebe Lin <PhoebeLin2001@gmail.com>
2021-03-25 21:29:56 -0400

    search bar

--- TRT-django/marketplace/static/marketplace/functions.js
+++ TRT-django/marketplace/static/marketplace/functions.js
@@ -1,15 +1,13 @@
 function search(){
-   var ret_items = []
+   var ret_items = [];
+   console.log('yo');
    query = document.getElementById("searchbar").value;
-   /*
-   item=[{% for item in items %}
-            {{item.name}, }
-         {% endfor %}]
-   if (query == null)
-      return items; */
-   for (i = 0; i < items.length; i++) {
-      if(query.equals(items[i])) {
-         ret_items.push(items[i]);
-      }
+   cards = document.getElementsByClassName("card");
+   // var item=[{% for item in items %} {{item.name}, } {% endfor %}];
+   
+   for (i = 0; i < cards.length; i++) {
+      // console.log(cards[i].getElementsByClassName("card-title"))
+      if (!cards[i].querySelector("h5").innerHTML.includes(query))
+         cards[i].style.display='none'
    }
 }
