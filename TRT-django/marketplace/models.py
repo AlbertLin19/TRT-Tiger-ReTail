eb9b08889d67a3a08f6dc2331ff72b04d2669236
Albert Lin <aklin@princeton.edu>
2021-03-26 13:08:50 -0400

    migrate

--- TRT-django/marketplace/models.py
+++ TRT-django/marketplace/models.py
@@ -47,7 +47,7 @@ class Item(models.Model):
     name = models.CharField(max_length=50, verbose_name="Item Name")
     posted_date = models.DateTimeField()
     deadline = models.DateField(
-        help_text="Latest allowed is " + (timezone.now() + MAX_TIME_DELTA).date(),
+        help_text="Latest allowed is " + str((timezone.now() + MAX_TIME_DELTA).date()),
         verbose_name="Deadline to Sell",
         validators=[
             MinValueValidator(timezone.now().date()),
