b767337faa240ea51c000325420c4e2d33f44853
Phoebe Lin <PhoebeLin2001@gmail.com>
2021-04-02 15:25:09 -0400

    changes

--- TRT-django/marketplace/static/marketplace/functions.js
+++ TRT-django/marketplace/static/marketplace/functions.js
@@ -1,16 +1,14 @@
 function search(){
-   var ret_items = [];
-   console.log('yo');
    query = document.getElementById("searchbar").value.toLowerCase();
    cards = document.getElementsByClassName("card");
-   // var item=[{% for item in items %} {{item.name}, } {% endfor %}];
    if (query != '') {
       for (i = 0; i < cards.length; i++) {
-         // console.log(cards[i].getElementsByClassName("card-title"))
-         if (!cards[i].querySelector("h5").innerHTML.toLowerCase().includes(query))
-            cards[i].style.display='none';
-         else
+         if (cards[i].querySelector(".item_categories").innerHTML.toLowerCase().includes(query))
             cards[i].style.display='';
+         else if (cards[i].querySelector("h5").innerHTML.toLowerCase().includes(query))
+            cards[i].style.display='';
+         else
+            cards[i].style.display='none';
       }
    }
    else {
@@ -19,3 +17,67 @@ function search(){
       }
    }
 }
+
+function filter(input){
+   //query = document.getElementById("filter").value.toLowerCase();
+   console.log(input)
+   cards = document.getElementsByClassName("card");
+   if (input != '') {
+      for (i = 0; i < cards.length; i++) {
+         if (cards[i].querySelector(".item_categories").innerHTML.toLowerCase().includes(input))
+            cards[i].style.display='';
+         else if (cards[i].querySelector("h5").innerHTML.toLowerCase().includes(input))
+            cards[i].style.display='';
+         else
+            cards[i].style.display='none';
+      }
+   }
+   else {
+      for (i = 0; i < cards.length; i++) {
+         cards[i].style.display='';
+      }
+   }
+}
+
+$('.carousel-control.left').click(function() {
+   $('#myCarousel').carousel('prev');
+ });
+ 
+ $('.carousel-control.right').click(function() {
+   $('#myCarousel').carousel('next');
+ });
+/*
+function imageScroll(counter){
+   images = document.getElementsByClassName("imageAlbum");
+   title_image = document.getElementsByClassName("headImage");
+   console.log(title_image)
+   console.log(images.length)
+   counter++;
+   for (i = 0; i < images.length; i++) {
+      if (counter===0)
+         title_image.style.display='inline';
+      else if (counter===i) {
+         //title_image.style.display='block';
+         images[i].style.display='inline';
+      }
+      else
+         images[i].style.display='none';
+   }
+   console.log(images.length)
+   console.log(images)
+   if (counter > images.length)
+      counter=0;
+   console.log(counter)
+   return counter;
+   console.log(counter)
+}
+
+<div style="display:center;" class="headImage">
+                                        {% cloudinary item.image format="jpg" width=240 height=160 crop="fill"%}
+                                    </div>
+                                    {%for image in item.album.all%}
+                                        <div style="display:none;" class="imageAlbum">
+                                            {% cloudinary image.image format="jpg" width=240 height=160 crop="fill"%}
+                                        </div>
+                                    {%endfor%}
+                                    <button onclick="counter=imageScroll(counter);">YO</button> */
