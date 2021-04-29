393b51af0bcb3a8f4e3e56c521c215459a80e3a4
Albert Lin <aklin@princeton.edu>
2021-04-29 19:28:50 -0400

    added rdondero as admin netid

--- TRT-django/TRT/settings.py
+++ TRT-django/TRT/settings.py
@@ -169,7 +169,7 @@ CACHES = {
 }
 
 # list of netids for which we allow multi-accounts
-ADMIN_NETIDS = ["aklin", "satadals", "singl", "kjm3", "sarats"]
+ADMIN_NETIDS = ["aklin", "satadals", "singl", "kjm3", "sarats", "rdondero"]
 # list of alt account suffixes for admins
 ALT_ACCOUNT_SUFFIXES = ["", "_alt_a", "_alt_b"]
 # time buffer after which expired items are deleted
